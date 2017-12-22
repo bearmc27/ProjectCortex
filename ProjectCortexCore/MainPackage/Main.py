1d5d8f7adae1d2add27bac0536ee48c68ea3ca2a
Man Kin Hung Bear <man_kinhung@hotmail.com.hk>
2017-12-23 01:45:59 +0800

    tested GUI video streaming

---
+++ ProjectCortexCore/MainPackage/Main.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+"""
+Objective:
+- Kick-starting the program.
+- Read configuration file and pass it to other class
+"""
+from TrackingSystem import TrackingSystem as tracking_sys
+
+# GUI testing
+from MainPackage.MainGUI import PhotoBoothApp
+from imutils.video import VideoStream
+import argparse
+import time
+
+
+def main():
+    # tracking_system = tracking_sys.TrackingSystem()
+
+    # initialize the video stream and allow the camera sensor to warmup
+    print("[INFO] warming up camera...")
+    # src=0 mean webcam device 0, i believe similar to cv2.VideoCapture(0)
+    vs = VideoStream(src = 0).start()
+    time.sleep(2.0)
+
+    # start the app
+    pba = PhotoBoothApp(vs)
+    pba.root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
